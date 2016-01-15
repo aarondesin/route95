af4f7fa39a669ae51b682fc1b77c1282d071d6ff
marco_iskandar <mitamzil@gmail.com>
2016-01-14 16:36:21 -0800

    musicmanager
    added empty lists and ints for future use

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+public class MusicManager{
+
+	public enum Key{
+		CMajor
+		CSharp
+		DMajor
+		DSharp
+		EMajor
+
+	};
+
+	public Key currentkey;
+
+
+	
+	/*public static void Searchkey(Key currentkey){
+		for(int i = 0; i <= 4; i++){
+			if(currentkey == Key.i) // doesn't work but trying to go through index of Key
+		}*/
+	
+	List<Riff>riffs = new riffs[currentkey] 
+	List<List<Riff>>song = new song[riffs]
+
+
+	}
+
+	public static float tempo = 120f;
+	private float beattimer;
+	private int beat;
+	
+
+
+
+}
