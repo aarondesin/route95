d3bbfea18c2eccacf3812ce1176f22b98e004c16
RichardP <rdpittma@ucsc.edu>
2016-01-24 17:11:58 -0800

    Terrain Manager script and object
    Converted Terrain Generator into a Terrain Manager.  Will create terrain
    and also manipulate it along with doodads.

---
+++ Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainManager : MonoBehaviour {
+	public int SIZE;
+	public int LINEAR_RESOLUTION;
+	public Material TERRAIN_MATERIAL;
+
+	private DynamicTerrain terrain;
+
+	// Use this for initialization
+	void Start () {
+		terrain = new DynamicTerrain (SIZE, LINEAR_RESOLUTION, TERRAIN_MATERIAL);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
