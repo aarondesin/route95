05405252ee3c6e9b3c0b3c3263b78b4fcfb2faa6
marco_iskandar <mitamzil@gmail.com>
2016-01-27 18:35:53 -0800

    basic riff code and start of riff ui

--- Assets/Scripts/Riff.cs
+++ Assets/Scripts/Riff.cs
@@ -3,6 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Riff {
-	//List<Note> notes = new List<Note>();
+
+	public enum Instrument{
+		Drums,
+		Guitar,
+		Bass
+		
+	};
+
+	List<Note> notes = new List<Note>();
+	public boolean play = true; // if note is playing at certain points
+	public boolean pause = true; // if player is looping the riff or just want silent
+				
+	public static void Sounds(Key currentInstrument, Key currentKey){
+		switch (currentInstrument) {
+		case Drums:
+
+			for (int i = 0; i < numSounds; i++) {
+				notes<i> = soundList [0, i];
+			}
+
+
+			break;
+		case Guitar:
+
+
+			break;
+		case Bass:
+			
+			break;
+			
+		}
+	}
+	
+	public static void checkPlay(boolean select){
+		if(select)
+
+		}
 }
 
---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Song : MonoBehaviour {
+
+
+	List<songpiece> song = new List<songpiece>(); 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
