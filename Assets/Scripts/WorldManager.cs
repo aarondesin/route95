9bf327be4fc9b80e30f59f9ea894b2ac9e806dc4
RichardP <rdpittma@ucsc.edu>
2016-01-29 02:59:56 -0800

    Added Sun and Moon functionality
    Sun and moon orbit terrain.
    Tweakable distance and position functions to get different inclinations.
    Tweakable colors to Lerp between (Dawn, Noon, Dusk, Midnight).

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -6,11 +6,34 @@ public class WorldManager : MonoBehaviour {
 	public int LINEAR_RESOLUTION;
 	public Material TERRAIN_MATERIAL;
 
+	public float TIME_SCALE;
+	public float LIGHT_X_SCALE;
+	public float LIGHT_Y_SCALE;
+	public float LIGHT_Z_SCALE;
+
 	private DynamicTerrain terrain;
+	private GameObject sun;
+	private GameObject moon;
 
 	// Use this for initialization
 	void Start () {
 		terrain = new DynamicTerrain (SIZE, LINEAR_RESOLUTION, TERRAIN_MATERIAL);
+
+		sun = new GameObject ("Sun");
+		sun.AddComponent<Light> ();
+		sun.AddComponent<Sun> ();
+		if (TIME_SCALE != 0) 
+			sun.GetComponent<Sun> ().setTimeScale (TIME_SCALE);
+		sun.GetComponent<Sun> ().setPosScales (LIGHT_X_SCALE, LIGHT_Y_SCALE, LIGHT_Z_SCALE);
+		sun.GetComponent<Light> ().shadows = LightShadows.Soft;
+
+		moon = new GameObject ("Moon");
+		moon.AddComponent<Light> ();
+		moon.AddComponent<Moon> ();
+		if (TIME_SCALE != 0)
+			moon.GetComponent<Moon> ().setTimeScale (TIME_SCALE);
+		moon.GetComponent<Moon> ().setPosScales (LIGHT_X_SCALE, LIGHT_Y_SCALE, LIGHT_Z_SCALE);
+		moon.GetComponent<Light> ().shadows = LightShadows.Soft;
 	}
 	
 	// Update is called once per frame
