31e8b7932dd47c6a550bb4374b90b7d743ca4f92
marco_iskandar <mitamzil@gmail.com>
2016-01-29 18:10:13 -0800

    update note.cs

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,20 +1,21 @@
 using UnityEditor;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;// need for using lists
 
-public class MusicManager{
+public class MusicManager : MonoBehaviour {
 	public enum Key{
-		CMajor,
-		CSharp,
+		DFlat,
 		DMajor,
-		DSharp,
-		EMajor
+		EMajor,
+		EFlat,
+		FMajor
 
 	};
 
 
 
-	public Key currentKey = Key.CMajor;
+	public Key currentKey = Key.EMajor;
 	public Riff.Instrument currentInstrument = Riff.Instrument.Drums;// value will be passed from key button
 
 	/*public static void Searchkey(Key currentkey){
@@ -32,25 +33,3 @@ public class MusicManager{
 }
 
 
-/*public void Play (int pos) {
-	//foreach (MusicManager.Key hit in riff[pos]) {
-	//MusicManager.PlayInstrument (hit);
-	//}
-}*/
-
-	/*public static void PlayInstrument (Riffs hit) {
-		switch (hit) {
-		case Percussion.Kick:
-			kick.Play();
-			break;
-		case Percussion.Tom:
-			tom.Play();
-			break;
-		case Percussion.Snare:
-			snare.Play();
-			break;
-		case Percussion.Hat:
-			hat.Play();
-			break;
-		}
-	}*/
--- Assets/Scripts/Riff.cs
+++ Assets/Scripts/Riff.cs
@@ -11,11 +11,12 @@ public class Riff {
 		
 	};
 
-	List<Note> notes = new List<Note>();
+	List<Note> notes = new List<Note>(); // contains notes
 	 
 	public bool pause = true; // if player is looping the riff or just want silent
-	public MusicManager.Key currentKey = MusicManager.Key.CMajor;
-				
+	public MusicManager.Key currentKey = MusicManager.Key.EMajor;
+	public static int drumRiffIndex = 0;
+
 	void Sounds(Instrument currentInstrument, MusicManager.Key currentKey){
 		switch (currentInstrument) {
 		case Instrument.Drums:
@@ -40,9 +41,17 @@ public class Riff {
 
 
 
-	public void playriff(){
+	public void playriff(int pos){ // plays all the notes within the sequencer aka the riff 
+		notes[drumRiffIndex].Play(pos);
 	
 	}
+
+	
+	/*public void Play (int pos) {
+		foreach (Instrument hit in riff[pos]) {
+			MusicManager.PlayPercussion (hit);
+		}
+	}*/
 	
 	public static void checkPlay(bool select){
 		if (select) {
