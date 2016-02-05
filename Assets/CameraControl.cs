c04ca9f5440dab68fe0a67c7661063bbdbc9fc4c
aarondesin <adesin@ucsc.edu>
2016-02-04 22:22:43 -0800

    Basic First Person Camera, World Space UI
    - Camera now interps between positions when menu switches
    - Song Management UI is now placed on car radio

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform _initialPosition;
+
+	Transform _startPosition;
+	Transform _targetPosition;
+	public float _speed;
+	float _sTime;
+
+	public void SetSpeed (float newSpeed) {
+		_speed = newSpeed;
+	}
+
+	public void SnapToPosition (Transform newPosition) {
+		_startPosition = newPosition;
+		_targetPosition = newPosition;
+		GetComponent<Transform>().position = newPosition.position;
+		GetComponent<Transform>().rotation = newPosition.rotation;
+	}
+
+	public void MoveToPosition (Transform newPosition) {
+		_startPosition = GetComponent<Transform>();
+		_sTime = Time.time;
+		_targetPosition = newPosition;
+	}
+
+	void Update() {
+		if (Vector3.Distance(_startPosition.GetComponent<Transform>().position, _targetPosition.GetComponent<Transform>().position) != 0f) {
+			GetComponent<Transform>().position = Vector3.Slerp(_startPosition.position, _targetPosition.position, 
+				(Time.time-_sTime)*_speed*Time.deltaTime/Vector3.Distance(_startPosition.GetComponent<Transform>().position, _targetPosition.GetComponent<Transform>().position));
+			GetComponent<Transform>().rotation = Quaternion.Slerp(_startPosition.rotation, _targetPosition.rotation, 
+				(Time.time-_sTime)*_speed*Time.deltaTime/Vector3.Distance(_startPosition.GetComponent<Transform>().position, _targetPosition.GetComponent<Transform>().position));
+		} else {
+			_startPosition = _targetPosition;
+		}
+	}
+
+	void Start() {
+		SnapToPosition (_initialPosition);
+	}
+}
