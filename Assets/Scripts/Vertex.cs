c3030920b409ed711c1e2461ec84a05c45ad3091
RichardP <rdpittma@ucsc.edu>
2016-04-19 01:07:54 -0700

    Vertex Script
    Base code of Vertex Class

---
+++ Assets/Scripts/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Vertex {
+	public static Dictionary<int, Dictionary<int, Vertex>> Vertices; 
+	float height;
+	float distToRoad;
+	public Vertex () {
+
+	}
+}
