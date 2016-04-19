8a803f0f45d663b0c28a1e35ac55b7ebb31d4fac
RichardP <rdpittma@ucsc.edu>
2016-04-19 02:27:22 -0700

    Vertex Class and Chunk Lerp
    Completed the Vertex Class and added the beginnings of lerping for chunk
    mesh vertices to their desired height.

--- Assets/Scripts/Vertex.cs
+++ Assets/Scripts/Vertex.cs
@@ -4,9 +4,25 @@ using System.Collections.Generic;
 
 public class Vertex {
 	public static Dictionary<int, Dictionary<int, Vertex>> Vertices; 
+	int x;
+	int y;
 	float height;
 	float distToRoad;
-	public Vertex () {
 
+	static Vertex() {
+		Vertices = new Dictionary<int, Dictionary<int, Vertex>>();
+	}
+
+	public Vertex (int x, int y, float height) {
+		this.x = x;
+		this.y = y;
+		this.height = height;
+
+		if (!Vertices.ContainsKey (x)) {
+			Vertex.Vertices.Add (x, new Dictionary<int, Vertex> ());
+		}
+		if (!Vertices [x].ContainsKey (y)) {
+			Vertex.Vertices [x].Add (y, this);
+		} 
 	}
 }
